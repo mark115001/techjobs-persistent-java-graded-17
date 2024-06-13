5d179ffd061ba6b14d324c097f4f5998d9806004
mark115001 <145294877+mark115001@users.noreply.github.com>
2024-06-13 08:25:20 -0500

    Prior to Initial Task 4 Work2

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -13,9 +13,6 @@ public class Skill extends AbstractEntity {
     @Size(max = 501, message = "Description to long")
     private String description;
 
-    @ManyToMany(mappedBy = "skills")
-    private List<Job> jobs = new ArrayList<>();
-
     public Skill() {}  //  Constructor for Hibernate
 
     public Skill(String description) {
@@ -30,11 +27,4 @@ public class Skill extends AbstractEntity {
         this.description = description;
     }
 
-    public List<Job> getJobs() {
-        return jobs;
-    }
-
-    public void setJobs(List<Job> jobs) {
-        this.jobs = jobs;
-    }
 }
